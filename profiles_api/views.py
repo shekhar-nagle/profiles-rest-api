ed21f748ce571d4333090387027a73a133c6fa5e
shekhar-nagle <shekharnnagle@gmail.com>
2022-11-30 13:10:50 +0530

    Create hello world api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiVIew(APIView):
+
+    def get(self, request, format=None):
+
+        an_api = [
+
+        "Uses Statndard HTTP methods as function(get, post,put,delete)",
+
+        "It is similar to traditional Django view",
+
+        "Gives you control over ur application and logic",
+
+        "It mapped manually to URL's"
+
+        ]
+
+
+        return Response({"message":"hello world", "an_api" : an_api})
